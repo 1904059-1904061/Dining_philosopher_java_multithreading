b29f4866863cc406b84d00eefd866132008fd4d6
SA-K1B <tareqshakib789@gmail.com>
2024-09-24 20:55:49 +0600

    labeling the philosophers

--- src/Runsimulation.java
+++ src/Runsimulation.java
@@ -1,6 +1,7 @@
 import java.io.*; 
 import java.util.*; 
 public class Runsimulation {
+    private int time_passed = 0;
     private int num_tables;
     private int philosophers_per_table;
     // private String[] philsopher_label = new String[25];
@@ -9,6 +10,7 @@ public class Runsimulation {
         this.philosophers_per_table = 5;
     }
     public void startSimulation(){
+    Thread thread_for_time = new Thread(this::simulate);
     Table[] tables = new Table[num_tables+1];
     for (int i=1;i<=num_tables;i++){
         tables[i] = new Table(i);
@@ -19,5 +21,14 @@ public class Runsimulation {
         }
     }
   }
-   
+  private void simulate() {
+    while (true) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        time_passed++;
+    }
+  }
 }
