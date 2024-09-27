ed363ed94457a02ff437fd68c946b6b18bc18ccf
SA-K1B <tareqshakib789@gmail.com>
2024-09-27 23:23:15 +0600

    table class updated

--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,6 @@
+import java.util.concurrent.Semaphore;
 public class Table {
+   private final Semaphore[] forks = new Semaphore[5];
     private int id;
     public Table(int id){
       this.id = id;
@@ -6,4 +8,22 @@ public class Table {
     public int getId(){
       return this.id;
     }
+    public boolean fork_available(String philosopherLabel) throws InterruptedException {
+          int leftFork = philosopherLabel.charAt(0) % 5;
+          int rightFork = (leftFork + 1) % 5;
+          if (forks[leftFork].tryAcquire()) {
+            try {
+                
+                if (forks[rightFork].tryAcquire()) {     // trying to get the right fork
+                    System.out.println(philosopherLabel + " picked up both forks.");
+                    return true;  // when he gets both forks
+                } else {
+                    forks[leftFork].release();
+                }
+            } finally {
+                forks[leftFork].release();
+            }
+        }
+        return false; 
+  }
 }
